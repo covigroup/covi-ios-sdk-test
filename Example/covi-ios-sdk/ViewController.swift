00492e8d799d50ed106bd63ab942699395355db8
covi-ensil <ensil@covi.co.kr>
2024-06-12 10:19:15 +0900

    Initial commit

---
+++ Example/covi-ios-sdk/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  covi-ios-sdk
+//
+//  Created by covi-ensil on 06/12/2024.
+//  Copyright (c) 2024 covi-ensil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
